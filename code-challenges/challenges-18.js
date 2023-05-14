fab0782bb7cc4851e774faff6c3f6c70a55307a3
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-05-14 19:43:44 +0000

    finished

---
+++ code-challenges/challenges-18.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Given a string find the length of the middle word in the sentence
+//
+// Ex:
+// Input: "You can't handle the truth!"
+// Output: 6
+//
+// Input: "You're gonna need a bigger boat"
+// Output: 1
+//
+
+const wordLength = (str) => {
+    // write your code here
+    let middleWord
+    let arr=str.split(" ")
+  let strLength=(str.split(" ").length)
+  strLength %2 ===0? middleWord=(strLength/2)+1: middleWord=(strLength/2)+0.5
+ console.log( middleWord)
+ return arr[middleWord-1].length
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes two strings and checks if the two strings contain the same combination of letters
+//
+// Ex:
+// Input: "dad", "add"
+// Output: true
+//
+// Input: "dad", "aadd"
+// Output: false
+//
+// Input: "table", "label"
+// Output: false
+//
+// Input: "cat", "rat"
+// Output: false
+
+const wordLetters = (str1, str2) => {
+    // write your code here
+    if (str1.length !== str2.length) {  
+        return false;
+      }
+      for (let i = 0; i < str1.length; i++) {  
+        let index = str2.indexOf(str1[i]);  
+        if (index === -1) {  //if the letter in str1 is not in str2
+          return false;
+        }
+        str2 = str2.slice(0, index) + str2.slice(index + 1); // remove the letter from str2
+      }
+      return true;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes an integer and a sorted array
+// The function returns the index of the integer in the array
+// If the integer was not there, the function returns where it could have been
+//
+// Ex:
+// Input: [1,3,5,6], 5
+// Output: 2
+//
+// Input: [1,3,5,6], 2
+// Output: 1
+//
+// Input: [1,3,5,6], 7
+// Output: 4
+//
+
+const targetIndex = (arr, int) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { wordLength, wordLetters, targetIndex };
